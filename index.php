311720cad49e2b4d317830fd03449d898a721154
muac404 <muac404@gmail.com>
2023-01-10 17:45:50 +0100

    Aggiunge extends

--- index.php
+++ index.php
@@ -23,11 +23,45 @@ class Product {
     }
 }
 
+class Cane extends Product {
+    public $type;
+    public function setType($type)
+    {
+            $this->type = $type;
+    }
+
+    public function getType()
+    {
+        return $this->type;
+    }
+}
+
+class Gatto extends Product {
+    public $type;
+    public function setType($type)
+    {
+            $this->type = $type;
+    }
+
+    public function getType()
+    {
+        return $this->type;
+    }
+}
+
+// Prodotto normale
 $croccantini = new Product("Croccantini Veg", "€12.90","dog");
 $gioco = new Product("Pallina Giocattolo", "€13.75", "dog&cat");
 $cuccia = new Product("Cuccia Rigida", "€44.75", "dog");
 $cuccia2 = new Product("Cuccia Morbida ", "€64.75", "cat");
 $pettorina = new Product("Julius K9 - Pettorina Taglia Baby 1 / XS (30-40 cm)", "€17.90", "dog");
 
-var_dump($croccantini, $gioco, $cuccia, $cuccia2,$pettorina);
+// Cane
+$dogcollar = new Cane ("Pettorina K76", "€67.90","dog");
+
+// Gatto
+$collar = new Gatto ("Collarino Borchiato", "€77.90","cat");
+
+
+var_dump($croccantini, $gioco, $cuccia, $cuccia2,$pettorina,$collar,$dogcollar);
 ?>
